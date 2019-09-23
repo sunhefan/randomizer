145d5dd24aca385ef5a83dd94aba40585aadc679
HeFan Lim <hefanlim@HeFans-MacBook-Pro.local>
2019-09-23 10:06:21 -0700

    test
    test

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
+//let user = {};
+
 function setup() {
   createCanvas(400, 400);
-}
 
+}
 function draw() {
   background(220);
 
-  text("this is Lydia", 70, 70);
+ text("this is Lydia", 70, 70);
+  // text("HALOA, this is Wei", 70, 100);
 
-  text("HALOA, this is Wei", 70, 100);
 }
+
+//let user = {
+firstName: "Lydia",
+lastName: "Lim",
+favoriteColor: "blue",
+favoriteAnimal : "dog",
+book: "No",
+movie: "me before you",
+game: "No",
+superhero: "Thor",
+randomFact: " birds are tetrapods even though they only walk on two legs",
+};
